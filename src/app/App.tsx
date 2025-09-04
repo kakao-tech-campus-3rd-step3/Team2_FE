71f40abb134f4e5ae7138d2e59eb6db2bef8f287
anseonghyeon <anseonghyeon@gmail.com>
2025-09-04 21:46:31 +0900

    refactor: App.tsx에 AppRoutes를 상대경로로 사용하고 있던것을 alias를 사용하도록 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes';
+import AppRoutes from '@/app/routes/AppRoutes'
 
 function App() {
   return (
