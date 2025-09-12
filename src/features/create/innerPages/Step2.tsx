57e258d3656df3dc8ac58f04e923519520c9dd72
changhee-cho <changhee@kangwon.ac.kr>
2025-09-12 12:55:29 +0900

    fix: vercel 배포 오류 임시 개선

--- src/features/create/innerPages/Step2.tsx
+++ src/features/create/innerPages/Step2.tsx
@@ -6,12 +6,14 @@ import styled from '@emotion/styled';
 interface Step2Props {
   onValidChange: (isValid: boolean) => void;
 }
+
 const InfoContainer = styled.div`
   display: flex;
   justify-content: center;
   width: 100%;
   gap: 4px;
 `;
+
 const SettingInfoBox = styled.div`
   background-color: ${({ theme }) => theme.colors.background.foreground};
   border-radius: ${({ theme }) => theme.radius.radius2};
@@ -22,26 +24,31 @@ const SettingInfoBox = styled.div`
   flex-direction: column;
   padding: 15px;
 `;
+
 const InfoTitle = styled.h3`
   width: 100%;
   font-size: ${({ theme }) => theme.typography.body3Bold.fontSize};
 `;
+
 const InfoContent = styled.h4`
   width: 100%;
   font-size: ${({ theme }) => theme.typography.title1Bold.fontSize};
   padding: 25px 0px;
 `;
+
 const InfoSpan = styled.span`
   width: 100%;
   font-size: ${({ theme }) => theme.typography.body3Regular.fontSize};
   color: ${({ theme }) => theme.colors.gray.gray6};
 `;
+
 const CreateButtonContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
 `;
-const CreateButton = styled.button`
+
+const CreateButton = styled.button<{ disabled?: boolean }>`
   background-color: ${({ theme, disabled }) =>
     disabled ? theme.colors.gray.gray5 : theme.colors.semantic.primary};
   border-radius: ${({ theme }) => theme.radius.radius1};
@@ -51,18 +58,39 @@ const CreateButton = styled.button`
   font-weight: ${({ theme }) => theme.typography.body2Bold.fontWeight};
   cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 `;
+
 const Spacer12 = styled.div`
   height: 12px;
 `;
+
 const Spacer20 = styled.div`
   height: 20px;
 `;
 
+const infoData = [
+  {
+    title: '선택된 PDF',
+    content: '1개',
+    description: '개발자 면접 가이드.pdf',
+  },
+  {
+    title: '문제 개수',
+    content: '20문제',
+    description: 'PDF 내용으로만',
+  },
+  {
+    title: '문제 유형',
+    content: '객관식',
+    description: '4지 선다형',
+  },
+];
+
 const Step2: React.FC<Step2Props> = ({ onValidChange }) => {
   const [selectedFileId, setSelectedFileId] = useState<string | null>(null);
+  // 현재는 다음 페이지가 비활성화되기에 set을 할 필요는 없으나, vercel 배포에서 에러를 내므로 임시로 사용 => 추후 수정 필요
+  setSelectedFildId(null);
 
   useEffect(() => {
-    // 예시: selectedFileId가 있으면 유효(true), 없으면 false
     onValidChange(!!selectedFileId);
   }, [selectedFileId, onValidChange]);
 
@@ -72,21 +100,13 @@ const Step2: React.FC<Step2Props> = ({ onValidChange }) => {
       <SubTitle>AI가 생성할 맞춤형 문제집을 확인해보세요</SubTitle>
       <Spacer12 />
       <InfoContainer>
-        <SettingInfoBox>
-          <InfoTitle>선택된 PDF</InfoTitle>
-          <InfoContent>1개</InfoContent>
-          <InfoSpan>개발자 면접 가이드.pdf</InfoSpan>
-        </SettingInfoBox>
-        <SettingInfoBox>
-          <InfoTitle>문제 개수</InfoTitle>
-          <InfoContent>20문제</InfoContent>
-          <InfoSpan>PDF 내용으로만</InfoSpan>
-        </SettingInfoBox>
-        <SettingInfoBox>
-          <InfoTitle>문제 유형</InfoTitle>
-          <InfoContent>객관식</InfoContent>
-          <InfoSpan>4지 선다형</InfoSpan>
-        </SettingInfoBox>
+        {infoData.map(({ title, content, description }, index) => (
+          <SettingInfoBox key={index}>
+            <InfoTitle>{title}</InfoTitle>
+            <InfoContent>{content}</InfoContent>
+            <InfoSpan>{description}</InfoSpan>
+          </SettingInfoBox>
+        ))}
       </InfoContainer>
       <Spacer20 />
       <CreateButtonContainer>
