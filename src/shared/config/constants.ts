ad1e39b256caefc618223820207455c5662313a6
anseonghyeon <anseonghyeon@gmail.com>
2025-09-18 23:31:35 +0900

    chore: constants.ts 파일을 하나로 합치고 전역에 사용된것을 수정

---
+++ src/shared/config/constants.ts
@@ -0,0 +1,16 @@
+
+// 메뉴 상수
+export const MENUS = {
+  DASHBOARD: '대시보드',
+  SOURCE: '학습소스 관리',
+  CREATE: '문제집 생성',
+  QUIZ: '나의 문제집',
+  WRONG: '오답노트',
+} as const;
+
+// 메뉴 상수 타입
+export type Menu = typeof MENUS[keyof typeof MENUS];
+
+// 레이아웃 상수
+export const MIN_WIDTH = '1024px';
+export const MIN_HEIGHT = '640px';
