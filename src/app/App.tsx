b44b17825fef62f302b2c12fda5b085f7b578502
anseonghyeon <anseonghyeon@gmail.com>
2025-09-18 22:58:23 +0900

    refactor: BrowserRouter를 main.tsx로 옴김

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+
 import AppRoutes from './routes/AppRoutes';
 
 function App() {
   return (
-    <Router>
       <AppRoutes />
-    </Router>
   );
 }
 
