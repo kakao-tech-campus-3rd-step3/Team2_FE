86cf464f6170942df133a2ccb557fb8c70750bc1
Seonghyeon An <anseonghyeon@gmail.com>
2025-09-19 13:41:14 +0900

    FTSK-(20,21,23): sse 적용, 점수산출 기능, 문제풀이페이지 api연결 (#25)
    ## PR 설명
    
    - sse를 적용해서 알림 기능을 구현하고 토스트 메시지를 띄움
    - 문제풀이페이지 api 연결
    - 점수산출 기능
    
    ## 작업 상세 내용
    
    - Home페이지에 들어가면 sse연결을 시도하고 문제 생성이 완료되면 서버로부터 알림이 와서 토스트 메시지를 띄움
    - 토스트 메시지를 누르면 생성된 문제에대한 문제풀이 페이지로 이동
    - 문제풀이 페이지에 사용되던 mock데이터를 지우고 api를 연결하면서 동시에 구조도 개선
    - 답안을 클릭하고 다음페이지로 넘어가도 하이라이팅이 남아있던 문제 수정
    
    ## 기타사항 / 참고사항
    
    - 재밌어요!

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,56 @@ import Source from './HomeSection/Source';
 import Create from './HomeSection/Create';
 import Quiz from './HomeSection/Quiz';
 import Wrong from './HomeSection/Wrong';
-import SolvePage from './HomeSection/Solve';
+import Solve from './HomeSection/Solve';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { createEventSource } from '@/shared/utils/sse';
+
+import { toast } from 'react-toastify';
 
 function Home() {
-  const [selectedMenu, setSelectedMenu] = useState('문제집 생성');
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState<boolean>(true); // LSB의 열림, 닫힘 state
+  const [selectedMenu, setSelectedMenu] = useState<string>('문제집 생성'); // TODO: 현재 state로 어는 페이지에 있는지 저장하고 페이징, 하지만 리액트 라우터 아울렛을 사용하는 방식으로 리펙토링 해야함
+  const [, setQuestionSetReady] = useState<boolean>(false); // 문제 생성이 완료되었는지 state, 구조 개선하면 지울수도 있는 state, questionSetReady 쓸때 구조분해할당에 추가
+  const [questionSetId, setQuestionSetId] = useState<number>(5); // 문제 조회할때 보낼 state, questionSetId 쓸때 구조분해할당에 추가
+
+  // sse 연결
+  useEffect(() => {
+    // sse 연결 부분
+    console.log('Home.tsx useEffect 진입');
+    const es = createEventSource({
+      onOpen: () => {
+        console.log('SSE Open');
+      },
+      onHandShake: () => {
+        console.log('SSE HandShake후 SSE 구독'); // 이 log가 뜨면 서버와 sse 연결이 된것
+      },
+      onQuestionSetCreationComplete: (payload) => {
+        // payload.success로 문제가 다 생성된것을 감지
+        // payload.questionSetId로 문제 조회할때 사용
+        // payload.message로 토스트 메시지 띄울때 사용
+
+        if (payload.success) {
+          console.log('문제집 생성 완료');
+          setQuestionSetReady(true);
+          setQuestionSetId(payload.questionSetId);
+          toast(payload.message, {
+            onClick: () => {
+              setSelectedMenu('문제풀이');
+            },
+          }); // message 내용으로 토스트 메시지를 띄움
+        } else {
+          console.log('문제집 생성 실패');
+        }
+      },
+    });
+
+    // TODO: 추후 로그아웃시 sse 연결 해제로 구현
+    return () => {
+      es?.close?.();
+    };
+  }, []);
+
   const renderContent = () => {
     switch (selectedMenu) {
       case '대시보드':
@@ -23,12 +66,12 @@ function Home() {
         return <Source />;
       case '문제집 생성':
         return <Create />;
+      case '문제풀이':
+        return <Solve setSelectedMenu={setSelectedMenu} questionSetId={questionSetId} />;
       case '나의 문제집':
         return <Quiz />;
       case '오답노트':
         return <Wrong />;
-      case '문제풀이':
-        return <SolvePage setSelectedMenu={setSelectedMenu} />;
       default:
         return <Dashboard />;
     }
@@ -36,6 +79,7 @@ function Home() {
 
   return (
     <AppLayout>
+      {/* 사이드바 */}
       <SideBar
         selectedMenu={selectedMenu}
         setSelectedMenu={setSelectedMenu}
@@ -43,12 +87,9 @@ function Home() {
         setIsOpen={setIsOpen}
       />
       <PageLayout>
-        <PageHeader
-          selectedMenu={selectedMenu}
-          setSelectedMenu={setSelectedMenu}
-          isOpen={isOpen}
-          setIsOpen={setIsOpen}
-        />
+        {/* 페이지 헤더 */}
+        <PageHeader selectedMenu={selectedMenu} isOpen={isOpen} setIsOpen={setIsOpen} />
+        {/* 페이지 컨텐츠 */}
         <PageContent>{renderContent()}</PageContent>
       </PageLayout>
     </AppLayout>
