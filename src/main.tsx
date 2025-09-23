63f36b33130580964c23e1440ace3fd255862fde
anseonghyeon <anseonghyeon@gmail.com>
2025-09-23 20:45:05 +0900

    refactor: ChakraProvider 위치 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,24 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '@/app/App.tsx';
-
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './shared/styles/theme';
-import '@/shared/styles/global.css';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify';
-import { BrowserRouter } from 'react-router-dom';
 
+import '@/shared/styles/global.css';
+import App from '@/app/App.tsx';
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider theme={theme}>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
           <App />
           <ToastContainer position="bottom-center" hideProgressBar={true} />
-        </ChakraProvider>
-      </QueryClientProvider>
+        </QueryClientProvider>
+      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>,
 );
