0fe530887c7461dc50a3d652c0df47e499a02181
Seonghyeon An <anseonghyeon@gmail.com>
2025-10-01 16:59:42 +0900

    FTSK-46: Lottie로 NotFound 페이지 생성 (#45)
    ## PR 설명
    
    - Lottie로 NotFound 페이지 생성
    
    ## 작업 상세 내용
    
    - 저희 풀잇 대표컬러로 로티를 만들고 NotFound페이지에 사용했습니다
    
    ## 기타사항 / 참고사항
    
    - 아직 좀 부족하긴한데 심심할때마다 수정할께요

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,19 +1,31 @@
 import styled from '@emotion/styled';
-import SampleLottie from '@/shared/assets/lotties/sample.lottie';
+import SampleLottie from '@/shared/assets/lotties/404 error.lottie';
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { Link } from 'react-router-dom';
 
 const NotFoundWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const BackBtn = styled.div`
+  background-color: ${({ theme }) => theme.colors.green.green6};
+  color: ${({ theme }) => theme.colors.gray.gray3};
+  padding: ${({ theme }) => theme.spacing.spacing4};
+  border-radius: ${({ theme }) => theme.radius.radius4};
 `;
 
 function NotFound() {
   return (
     <NotFoundWrapper>
-      <div style={{ width: 200, height: 200, margin: '0 auto' }}>
-        <DotLottieReact src={SampleLottie} loop autoplay />
+      <div>
+        <DotLottieReact style={{ width: 700, height: 500 }} src={SampleLottie} loop autoplay />
       </div>
+      <Link to="/dashboard">
+        <BackBtn>대시보드로 돌아가기</BackBtn>
+      </Link>
     </NotFoundWrapper>
   );
 }
