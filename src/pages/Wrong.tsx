e06b82c322fea269359df790d03e27ef65e73c28
anseonghyeon <anseonghyeon@gmail.com>
2025-10-15 22:12:29 +0900

    style: 코드 스타일 변경

--- src/pages/Wrong.tsx
+++ src/pages/Wrong.tsx
@@ -137,38 +137,38 @@ function Wrong() {
 
   return (
     // <PageLayout>
-      <WrongWrapper>
-        <ContentWrapper>
-          <WrongPageTitleWrapper>
-            <WrongPageTitle>오답노트</WrongPageTitle>
-          </WrongPageTitleWrapper>
-          <WrongPageDescription>
-            문제집별로 틀린 문제를 분석하고 완벽히 이해할 때까지 학습하세요
-          </WrongPageDescription>
-          <SearchBarWrapper>
-            <SearchBarDescription>
-              {filteredQuestionSets?.length}개의 오답이 검색되었습니다
-            </SearchBarDescription>
-            <SearchBar
-              placeholder="오답노트 제목으로 검색"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-          </SearchBarWrapper>
-          <WrongNoteList>
-            <WrongNoteListHeader>
-              <WrongNoteListHeaderColumn>문제집</WrongNoteListHeaderColumn>
-              <WrongNoteListHeaderColumn>오답 수</WrongNoteListHeaderColumn>
-              <WrongNoteListHeaderColumn>난이도</WrongNoteListHeaderColumn>
-              <WrongNoteListHeaderColumn>카테고리</WrongNoteListHeaderColumn>
-              <WrongNoteListHeaderColumn>작업</WrongNoteListHeaderColumn>
-            </WrongNoteListHeader>
-            {filteredQuestionSets?.map((item) => (
-              <WrongNoteListItem key={item.questionSetId} item={item} />
-            ))}
-          </WrongNoteList>
-        </ContentWrapper>
-      </WrongWrapper>
+    <WrongWrapper>
+      <ContentWrapper>
+        <WrongPageTitleWrapper>
+          <WrongPageTitle>오답노트</WrongPageTitle>
+        </WrongPageTitleWrapper>
+        <WrongPageDescription>
+          문제집별로 틀린 문제를 분석하고 완벽히 이해할 때까지 학습하세요
+        </WrongPageDescription>
+        <SearchBarWrapper>
+          <SearchBarDescription>
+            {filteredQuestionSets?.length}개의 오답이 검색되었습니다
+          </SearchBarDescription>
+          <SearchBar
+            placeholder="오답노트 제목으로 검색"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </SearchBarWrapper>
+        <WrongNoteList>
+          <WrongNoteListHeader>
+            <WrongNoteListHeaderColumn>문제집</WrongNoteListHeaderColumn>
+            <WrongNoteListHeaderColumn>오답 수</WrongNoteListHeaderColumn>
+            <WrongNoteListHeaderColumn>난이도</WrongNoteListHeaderColumn>
+            <WrongNoteListHeaderColumn>카테고리</WrongNoteListHeaderColumn>
+            <WrongNoteListHeaderColumn>작업</WrongNoteListHeaderColumn>
+          </WrongNoteListHeader>
+          {filteredQuestionSets?.map((item) => (
+            <WrongNoteListItem key={item.questionSetId} item={item} />
+          ))}
+        </WrongNoteList>
+      </ContentWrapper>
+    </WrongWrapper>
     // </PageLayout>
   );
 }
