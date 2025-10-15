0a0ff0ad788e62d96b26f7b3201173df72cd7b23
anseonghyeon <anseonghyeon@gmail.com>
2025-10-15 20:45:14 +0900

    fix: 문제 유형별로 대응하기위해 타입 수정

--- src/features/solve/types/MarkingRequest.ts
+++ src/features/solve/types/MarkingRequest.ts
@@ -1,5 +1,28 @@
-export type MarkingRequest = {
+// export type MarkingRequest = {
+//   questionId: number;
+//   memberAnswer: string;
+//   memberAnswerType: string;
+// };
+
+type MultiChoice = {
   questionId: number;
   memberAnswer: string;
-  memberAnswerType: string;
+  memberAnswerType: 'string';
 };
+
+type ShortAnswer = {
+  questionId: number;
+  memberAnswer: string;
+  memberAnswerType: 'string';
+};
+
+type SingleChoice = {
+  questionId: number;
+  memberAnswer: boolean;
+  memberAnswerType: 'boolean';
+};
+
+// 통합 타입 (세 가지 중 하나)
+export type MarkingRequest = MultiChoice | ShortAnswer | SingleChoice;
+
+
