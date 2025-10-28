8c4f2975284f149614d5aa613983715c68cf8298
changhee-cho <changhee@kangwon.ac.kr>
2025-10-28 23:55:49 +0900

    refactor: prettier 적용

--- src/features/create/components/PdfFileList.tsx
+++ src/features/create/components/PdfFileList.tsx
@@ -54,16 +54,16 @@ const FileListDivWithScroll = styled.div`
   overflow: auto;
   width: 100%;
   height: 30dvh;
-  min-height:200px;
-  max-height:700px;
+  min-height: 200px;
+  max-height: 700px;
   border-radius: ${({ theme }) => theme.radius.radius2};
 `;
 
 const LoadingDiv = styled.div`
   width: 100%;
   height: 30dvh;
-  min-height:200px;
-  max-height:700px;
+  min-height: 200px;
+  max-height: 700px;
   display: flex;
   justify-content: center;
   align-items: center;
