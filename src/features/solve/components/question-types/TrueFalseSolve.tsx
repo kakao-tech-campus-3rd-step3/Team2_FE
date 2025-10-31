2c08b9e69127d36bd9086ef792663d8071525e5d
anseonghyeon <anseonghyeon@gmail.com>
2025-10-31 15:43:32 +0900

    style: 코드 스타일 변경

--- src/features/solve/components/question-types/TrueFalseSolve.tsx
+++ src/features/solve/components/question-types/TrueFalseSolve.tsx
@@ -220,7 +220,9 @@ function TrueFalseSolve({
         {isExplanationPage && (
           <ExplanationBox>
             <ExplanationBoxTitle>정답 및 해설</ExplanationBoxTitle>
-            <AnswerTxt>정답 : {questions.questions[currentQuestionIndex - 1].answer ? "참" : "거짓"}</AnswerTxt>
+            <AnswerTxt>
+              정답 : {questions.questions[currentQuestionIndex - 1].answer ? '참' : '거짓'}
+            </AnswerTxt>
             <ExplanationTxt>
               {questions.questions[currentQuestionIndex - 1].explanation}
             </ExplanationTxt>
